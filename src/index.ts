e3223701f41b0f857bd7382168fe0e0d7a97a536
ducktrshessami <jwee761@gmail.com>
2023-03-20 21:15:57 -0700

    request age check implementation

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
-import { verifyRequest } from "./twitch";
+import { RequestHeaders, verifyRequest } from "./twitch";
+import { requestHeader } from "./utils";
 
 export interface Env {
 	TWITCH_SECRET: string;
+	TWITCH_AGE_WARNING?: string;
 }
 
 export default {
 	async fetch(
 		request: Request,
-		env: Env,
-		ctx: ExecutionContext
+		env: Env
 	): Promise<Response> {
 		const body = await request.blob();
 		if (await verifyRequest(request, body, env)) {
-
+			checkAge(request, env);
 		}
 		return new Response("Hello World!");
 	},
 };
+
+function checkAge(request: Request, env: Env): void {
+	const TWITCH_AGE_WARNING = env.TWITCH_AGE_WARNING ? parseInt(env.TWITCH_AGE_WARNING) : 300000;
+	const timestamp = new Date(requestHeader(request, RequestHeaders.MessageTimestamp));
+	const age = Date.now() - timestamp.getTime();
+	if (age > TWITCH_AGE_WARNING) {
+		// TODO: Age warning
+	}
+}
