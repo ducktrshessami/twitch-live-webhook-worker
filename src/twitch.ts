e0f9aea95a867ed21ef99d585e3b07a68508b538
ducktrshessami <jwee761@gmail.com>
2023-03-21 15:53:35 -0700

    declare challenge body type

--- src/twitch.ts
+++ src/twitch.ts
@@ -55,3 +55,41 @@ export async function verifyRequest(request: Request, body: Blob, env: Env): Pro
         message
     );
 }
+
+export type BaseCondition = { broadcaster_user_id: string };
+export type ChannelFollowCondition = BaseCondition & { moderator_user_id: string };
+export type ChannelRaidCondition = {
+    from_broadcaster_user_id: string;
+    to_broadcaster_user_id: string;
+};
+export type ChannelPointsCustomSpecificRewardCondition = BaseCondition & { reward_id: string };
+export type DropEntitlementGrantCondition = {
+    organization_id: string;
+    category_id: string;
+    campaign_id: string;
+};
+export type ExtensionBitsTransactionCreateCondition = { extension_client_id: string };
+export type UserAuthorizationCondition = { client_id: string };
+export type UserUpdateCondition = { user_id: string };
+export type Condition =
+    BaseCondition |
+    ChannelFollowCondition |
+    ChannelRaidCondition |
+    ChannelPointsCustomSpecificRewardCondition |
+    DropEntitlementGrantCondition |
+    ExtensionBitsTransactionCreateCondition |
+    UserAuthorizationCondition |
+    UserUpdateCondition;
+
+export type Subscription<T extends Condition = Condition> = {
+    id: string;
+    type: string;
+    version: string;
+    status: string;
+    cost: number;
+    condition: T;
+    created_at: string;
+};
+
+export type BaseWebhookBody = { subscription: Subscription };
+export type WebhookCallbackVerificationBody = BaseWebhookBody & { challenge: string };
