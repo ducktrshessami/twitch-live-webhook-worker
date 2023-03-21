cfffca7a7058a070933214fd49ea228d5627c184
ducktrshessami <jwee761@gmail.com>
2023-03-20 18:10:42 -0700

    initial read of request body for reuse

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { verifyRequest } from "./twitch";
+
 export interface Env {
 	TWITCH_SECRET: string;
 }
@@ -8,6 +10,10 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
+		const body = await request.blob();
+		if (await verifyRequest(request, body, env)) {
+
+		}
 		return new Response("Hello World!");
 	},
 };
--- src/twitch.ts
+++ src/twitch.ts
@@ -43,19 +43,19 @@ function requestHeader(request: Request, header: string): string {
     return request.headers.get(header) ?? "";
 }
 
-async function getHmacMessage(request: Request): Promise<ArrayBuffer> {
+async function getHmacMessage(request: Request, body: Blob): Promise<ArrayBuffer> {
     return await new Blob([
         requestHeader(request, RequestHeaders.MessageId),
         requestHeader(request, RequestHeaders.MessageTimestamp),
-        await request.blob()
+        body
     ])
         .arrayBuffer();
 }
 
-export async function verifyRequest(request: Request, env: Env): Promise<boolean> {
+export async function verifyRequest(request: Request, body: Blob, env: Env): Promise<boolean> {
     const [key, message] = await Promise.all([
         getKey(env),
-        getHmacMessage(request)
+        getHmacMessage(request, body)
     ]);
     return await crypto.subtle.verify(
         "HMAC",
